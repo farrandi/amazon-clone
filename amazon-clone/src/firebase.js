b62e709cc32bce6372e52e1f347985e76cc390d9
Farrandi Hernando <farrandi@Nandos-MacBook.local>
2022-05-04 16:01:17 -0700

    Setup the react-app with firebase

---
+++ amazon-clone/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyD6Pt428KZ7vPmZywyCXSPTSeq4ruKftR4",
+    authDomain: "clone-17c13.firebaseapp.com",
+    projectId: "clone-17c13",
+    storageBucket: "clone-17c13.appspot.com",
+    messagingSenderId: "1078919457557",
+    appId: "1:1078919457557:web:c86a775030b627ec09571c",
+    measurementId: "G-MSR323KQD7"
+  };
